0611020fc60cdd465ab20cb1890bec05311e8533
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2021-06-24 14:07:01 +0100

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,12 +2,7 @@ import { configure } from "@testing-library/react";
 import firebase from "firebase"
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyC7xF9dWn00DuBPu4uiBaNfG9i5kfZ8TYE",
-    authDomain: "netflix-clone-yt-12513.firebaseapp.com",
-    projectId: "netflix-clone-yt-12513",
-    storageBucket: "netflix-clone-yt-12513.appspot.com",
-    messagingSenderId: "10161194149",
-    appId: "1:10161194149:web:160f3397f981914aa1f94c"
+    
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
